861af8175511c2b273c136b71534519cf6e17133
pasindusampath <pasindub32@gmail.com>
2025-03-31 20:31:57 +0530

    response done

--- src/whatsapp/whatsapp.module.ts
+++ src/whatsapp/whatsapp.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { WhatsappController } from './whatsapp/whatsapp.controller';
 import { WhatsappService } from './whatsapp/whatsapp.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule], // Import HttpModule here
+  providers: [WhatsappService],
+  exports: [WhatsappService],
   controllers: [WhatsappController],
-  providers: [WhatsappService]
 })
 export class WhatsappModule {}
--- src/whatsapp/whatsapp/whatsapp.controller.ts
+++ src/whatsapp/whatsapp/whatsapp.controller.ts
@@ -1,8 +1,14 @@
-import { Controller, Get,Req } from '@nestjs/common';
+import { BadRequestException, Controller, Get,HttpCode,Post,Req } from '@nestjs/common';
 import { Request } from 'express';
+import { WhatsappService } from './whatsapp.service';
 
 @Controller('whatsapp')
 export class WhatsappController {
+    
+    constructor(
+        private readonly whatsappService:WhatsappService
+    ){}
+    
 
     @Get('test')
     getHello(): string {
@@ -26,5 +32,24 @@ export class WhatsappController {
         }
     }
 
+    @Post('webhook')
+    @HttpCode(200)
+    async handleIncomingWhatsappMessage(@Req() request:any): Promise<any> {
+        //const {messages} = request?.entry?.[0]?.changes?.[0].value ?? {};
+        const data = request.body;
+        if (!data) return;
+
+        const messages = data;
+        const messageData = messages.entry[0].changes[0].value.messages[0];
+        const sender = messageData?.from;
+        const messageId = messageData?.id;
+        console.log('message : '+JSON.stringify(messageData.text.body));
+        console.log('messageId : ',messageId);
+        switch(messageData?.type) {
+            case 'text':
+                await this.whatsappService.sendWhatsappMessage(sender);
+                break;
+        }
+    }
 
 }
